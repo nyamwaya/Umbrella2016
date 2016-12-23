f7e74e1fcfe3b7b52f28018a3e7629c679231b55
nyamwaya <alecksonnyamwaya@gmail.com>
2016-12-22 19:10:45 -0600

    create networking service

--- app/src/main/java/com/example/aleckson/umbrella/networking/model/WeatherService.java
+++ app/src/main/java/com/example/aleckson/umbrella/networking/model/WeatherService.java
@@ -1,9 +1,18 @@
 package com.example.aleckson.umbrella.networking.model;
 
+import com.example.aleckson.umbrella.networking.WeatherApi;
+
+import retrofit2.http.Path;
+import rx.Observable;
+
 /**
  * Created by kerub on 12/22/2016.
  */
 
-public class WeatherService {
-    
+public class WeatherService implements WeatherApi {
+
+    @Override
+    public Observable<WeatherResults> getWeather(@Path("zip") int zipCode) {
+        return null;
+    }
 }
