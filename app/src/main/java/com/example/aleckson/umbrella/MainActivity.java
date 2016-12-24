83673845da5c19c4ef1dbc3331b8dfd4d187dd65
nyamwaya <alecksonnyamwaya@gmail.com>
2016-12-23 18:22:52 -0600

    Create layout

--- app/src/main/java/com/example/aleckson/umbrella/MainActivity.java
+++ app/src/main/java/com/example/aleckson/umbrella/MainActivity.java
@@ -37,15 +37,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         weatherViewModel = new WeatherViewModel(new WeatherImpl(), AndroidSchedulers.mainThread());
     }
 
