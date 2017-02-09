5367faa6aad9e7b68854c29620838459448f929c
nyamwaya <alecksonnyamwaya@gmail.com>
2017-02-09 13:32:50 -0600

    Create model for the day

--- app/src/main/java/com/example/aleckson/umbrella/model/Day.java
+++ app/src/main/java/com/example/aleckson/umbrella/model/Day.java
@@ -1,7 +1,33 @@
 package com.example.aleckson.umbrella.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by kerub on 2/9/2017.
+ * Created by Aleckson on 2/9/2017.
+ * Represents a Day
  */
 public class Day {
+    public String dayName;
+    public List<DayConditions> conditions;
+    public Long yday;
+    private int highestTemp;
+    private int lowestTemp;
+
+    public int getHighestTemp() {
+        return highestTemp;
+    }
+
+    public void setHighestTemp(int highestTemp) {
+        this.highestTemp = highestTemp;
+    }
+
+    public int getLowestTemp() {
+        return lowestTemp;
+    }
+
+    public void setLowestTemp(int lowestTemp) {
+        this.lowestTemp = lowestTemp;
+    }
+
 }
