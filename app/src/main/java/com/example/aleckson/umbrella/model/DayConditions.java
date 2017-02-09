529af8f3ded2b41e09a837674e2de434db7705a3
nyamwaya <alecksonnyamwaya@gmail.com>
2017-02-09 13:33:17 -0600

    Create day's condition model

--- app/src/main/java/com/example/aleckson/umbrella/model/DayConditions.java
+++ app/src/main/java/com/example/aleckson/umbrella/model/DayConditions.java
@@ -1,7 +1,12 @@
 package com.example.aleckson.umbrella.model;
 
 /**
- * Created by kerub on 2/9/2017.
+ * Created by Aleckson on 2/9/2017.
+ * Represents A single days conditions
  */
 public class DayConditions {
+    public String condition;
+    public int tempC;
+    public String time;
+    public int tempF;
 }
